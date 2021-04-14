a862d86c6837f0ae18d2328f92b47f507d496063
Rose <qqbunny@icloud.com>
2021-04-14 17:01:28 +0800

    init

--- Kanahei Animation 兔兔要吃飯/ViewController.swift
+++ Kanahei Animation 兔兔要吃飯/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //製作漸層畫面
+
+        let gradientView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 926))
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = gradientView.bounds
+        gradientLayer.colors = [UIColor.orange.cgColor, UIColor.blue.cgColor]
+        gradientView.layer.addSublayer(gradientLayer)
+        view.layer.addSublayer(gradientLayer)
+        
+        // 載入動畫
+        guard let url = Bundle.main.url(forResource: "15281900@2x", withExtension: "png") else { return }
+        let cfUrl = url as CFURL
+        CGAnimateImageAtURLWithBlock(cfUrl, nil) { (_, cgImage, _) in
+            self.imageView.image = UIImage(cgImage: cgImage)
+       }
     }
 
 
